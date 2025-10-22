39071f21ca8e35676591543fccfcb5c8d22e42d0
bilelbzeouich <bzeouichbilel6@gmail.com>
2025-10-22 11:30:54 +0100

    last changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,84 +10,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        primarySwatch: Colors.blue,
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        leading: Icon(Icons.add_circle,color: Colors.amber,),
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'FlexStore',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          leading: const Icon(Icons.store, color: Colors.white),
+          actions: const [
+            Icon(Icons.search, color: Colors.white),
+            SizedBox(width: 16),
+            Icon(Icons.account_circle, size: 28, color: Colors.white),
+          ],
+          backgroundColor: Colors.blue,
+          iconTheme: const IconThemeData(color: Colors.white),
+        ),
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
       ),
-      body: Stack(
-        children: [
-          // Make the background image as the bottom layer
-          Positioned.fill(
-            child: Image.asset(
-              'assets/images/bg.jpeg',
-              fit: BoxFit.cover,
-              
-            ),
-          ),
-          // The content widgets on top of background image
-          Container(
-            color: Colors.white,
-            child: ListTile(
-              title: Text('Hello World', style: TextStyle(color: Colors.green , fontSize: 24, fontWeight: FontWeight.bold)),
-              subtitle: Text('Hello World', style: TextStyle(color: Colors.green , fontSize: 16, fontWeight: FontWeight.normal)),
-              leading: Icon(Icons.add_circle, color: Colors.amber),
-              trailing: Icon(Icons.add_box, color: Colors.amber),
-              onTap: () {
-                print('Hello World');
-              },
-            ),
-          ),
-          SizedBox(
-            height: 200,
-            child: Center(
-              child: Text('Hello World',
-                style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+    
     );
   }
 }
